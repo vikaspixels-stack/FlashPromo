506bbee136e9c98ee83d9a69c409a86c76a893c6
vikaspixels-stack <vikaspixels@gmail.com>
2026-08-14 17:08:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-const PASSWORD = 'FlashDemo2026';
-
-function ProtectedApp() {
-  const entered = prompt('Enter demo password');
-
-  if (entered !== PASSWORD) {
-    return (
-      <div style={{
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontFamily: 'Arial'
-      }}>
-        <h2>Access Denied</h2>
-      </div>
-    );
-  }
-
-  return <App />;
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
