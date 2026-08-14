0634c123efa2067815d3cbf4b4bb103567a685c4
vikaspixels-stack <vikaspixels@gmail.com>
2026-08-14 17:18:20 +0530

    Update main.jsx
    password protected

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const PASSWORD = 'KochiDemo2026';
+
+function ProtectedApp() {
+  const entered = window.prompt('Enter demo password');
+
+  if (entered !== PASSWORD) {
+    return (
+      <div style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Arial'
+      }}>
+        <h2>Access Denied</h2>
+      </div>
+    );
+  }
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
